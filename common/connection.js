980dc462349d402dffd2eb9d18715e2a14342959
Jatverma54 <jatinv94@gmail.com>
2020-12-28 03:53:23 +0530

    Modified Joined Group Section

--- common/connection.js
+++ common/connection.js
@@ -1,8 +1,8 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/groupAppdb', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
+mongoose.connect(process.env.mongoDB, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connectionasdf error:'));
  
 db.once('open', function() {
   //console.log("Successfully connected to MongoDB!");
-});
+});//||'mongodb://localhost:27017/groupAppdb'
