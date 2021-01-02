e6513443d6ad7b042dd282fa0a4f4668384e32ec
Jatverma54 <jatinv94@gmail.com>
2021-01-03 03:29:19 +0530

    Mod

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ app.use(function (err, req, res, next) {
   res.render('error');
 });
 
-//app.listen(3000);
-const port= process.env.port;//||3000;
+
+const port= process.env.PORT;//||3000;
 app.listen(port);
 module.exports = app;
